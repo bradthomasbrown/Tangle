90de051147fdc8fbfeff152ab33cec6dd7f4f1e1
Brad Brown <bradbrown@bradbrown.llc>
2023-01-12 12:45:56 -0500

    add GentleMidnight to Tangle

--- Tangle.sol
+++ Tangle.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.17;
 
 import '../ERC20/ERC20.sol';
 import '../Farmable/Farmable.sol';
+import '../GentleMidnight/GentleMidnight.sol';
 
 contract Tangle is
 ERC20,
-Farmable
+Farmable,
+GentleMidnight
 {}
