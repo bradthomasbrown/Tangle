9f7a1cf0a640424eb3efdcb1da5a81d7fc113859
Brad Brown <bradbrown@bradbrown.llc>
2023-01-18 02:47:30 -0500

    remapping changes

--- Tangle.sol
+++ Tangle.sol
@@ -2,16 +2,16 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.17;
 
-import '@ERC20/ERC20.sol';
-import '@Farmable/Farmable.sol';
-import '@GentleMidnight/GentleMidnight.sol';
+import './ERC20/ERC20.sol';
+import './Farmable/Farmable.sol';
+import './GentleMidnight/GentleMidnight.sol';
 
 contract Tangle is
 ERC20,
 Farmable,
 GentleMidnight
 {
-
+    
     address public owner;
 
     constructor() {
