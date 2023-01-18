7a959750db4a24d3d5fcd3c52e6bc93234de5788
Brad Brown <bradbrown@bradbrown.llc>
2023-01-18 14:39:33 -0500

    shutdown handling for HurtfulBeanstalk

--- testing/HurtfulBeanstalk/app.mjs
+++ testing/HurtfulBeanstalk/app.mjs
@@ -4,6 +4,7 @@ import { execSync } from 'child_process'
 import { SS } from './HelpfulCandlewax/index.mjs'
 
 function sendEth() {
+    if (this.request.url == '/shutdown') return
     let address = this.request.url.substring(1)
     let result = execSync(`geth --exec 'eth.sendTransaction({ from: eth.coinbase, to: "${address}", value: web3.toWei(1, "ether") })' attach http://localhost:8545`)
     this.data = result.toString().match(/"(.*)"/)[1]
