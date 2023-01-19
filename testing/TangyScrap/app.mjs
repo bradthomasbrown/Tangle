950dfeaa5a6b9cc64642994f83bed6a2f0aa7fd6
Brad Brown <bradbrown@bradbrown.llc>
2023-01-19 01:32:48 -0500

    Simulated user can now sell Tangle

--- testing/TangyScrap/app.mjs
+++ testing/TangyScrap/app.mjs
@@ -17,6 +17,9 @@ new class _ {
         this.weth = new Contract(WETH9.address, WETH9.abi, this.wallet)
         this.tangle = new Contract(Tangle.address, Tangle.abi, this.wallet)
         this.router = new Contract(UniswapV2Router02.address, UniswapV2Router02.abi, this.wallet)
+        await this.drink()
+        let tx = await this.tangle.approve(this.router.address, 2n << 128n)
+        await tx.wait()
         this.roll()
     }
 
@@ -24,7 +27,7 @@ new class _ {
         let roll = Math.random()
         switch (true) {
         case roll <= 0.45: await this.buy(); break
-        case roll <= 0.90: this.sell(); break
+        case roll <= 0.90: await this.sell(); break
         case roll <= 0.95: this.addLiquidity(); break
         case roll <= 1.00: this.removeLiquidity(); break }
         setTimeout(_ => { this.roll() }, ((Math.random() * 4) + 1) * 1000)
@@ -39,14 +42,23 @@ new class _ {
         let to = this.wallet.address
         let deadline = parseInt(Date.now() / 1000) + 60
         let value = BigInt(parseInt((Math.random() * 0.2 + 0.05) * balance))
-        let gas = await this.router.estimateGas.swapExactETHForTokens(amountOutMin, path, to, deadline, { value })
         let tx = await this.router.swapExactETHForTokens(amountOutMin, path, to, deadline, { value, gasLimit: 250000 })
         await tx.wait()
         console.log((await this.tangle.balanceOf(this.wallet.address)).toString())
     }
 
-    sell() {
+    async sell() {
         console.log('sell')
+        let balance = await this.tangle.balanceOf(this.wallet.address)
+        if (balance <= 0) return
+        let amountIn = BigInt(parseInt((Math.random() * 0.2 + 0.05) * balance))
+        let amountOutMin = 0
+        let path = [this.tangle.address, this.weth.address]
+        let to = this.wallet.address
+        let deadline = parseInt(Date.now() / 1000) + 60
+        let tx = await this.router.swapExactTokensForETH(amountIn, amountOutMin, path, to, deadline, { gasLimit: 250000 })
+        await tx.wait()
+        console.log((await this.tangle.balanceOf(this.wallet.address)).toString())
     }
 
     addLiquidity() {
