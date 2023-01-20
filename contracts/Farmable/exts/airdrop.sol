93ae690bc86db04f6cf3e5b72fabc1aae5844b2b
Brad Brown <bradbrown@bradbrown.llc>
2023-01-20 02:29:14 -0500

    add multi-airdrop support

--- contracts/Farmable/exts/airdrop.sol
+++ contracts/Farmable/exts/airdrop.sol
@@ -2,27 +2,35 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.17;
 
+import '../../ERC20/events/Transfer.sol';
 import '../../ERC20/ints/move.sol';
 import '../../ERC20/vars/balanceOf.sol';
 
 import '../ints/adjustPoints.sol';
-import '../mods/recipientZeroBalance.sol';
 import '../vars/generators.sol';
 
 contract hasExtAirdrop is
-hasModRecipientZeroBalance,
+hasEventTransfer,
+hasVarBalanceOf,
 hasVarGenerators
 {
 
-    function airdrop(address recipient)
+    function airdrop(address[] calldata recipients)
     external
-    recipientZeroBalance(recipient)
     {
         Generator storage generator = generators['tangle'];
         Farm storage farm = generator.farms['airdrop'];
         Account storage account = farm.accounts[msg.sender];
-        adjustPoints(generator, farm, account, 1);
-        move(balanceOf, msg.sender, recipient, 1e9);
+        uint count;
+        for (uint i = 0; i < recipients.length; i++) {
+            address recipient = recipients[i];
+            if (balanceOf[recipient] != 0) continue;
+            count++;
+            balanceOf[recipient] += 1e9;
+            emit Transfer(msg.sender, recipient, 1e9);
+        }
+        adjustPoints(generator, farm, account, int(count));
+        balanceOf[msg.sender] -= 1e9 * count;
     }
 
 }
