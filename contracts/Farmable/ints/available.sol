1265b68d1c3c5669591e9c4c71ab33de54453101
Brad Brown <bradbrown@bradbrown.llc>
2023-01-20 21:24:51 -0500

    severe math bug fix

--- contracts/Farmable/ints/available.sol
+++ contracts/Farmable/ints/available.sol
@@ -10,7 +10,7 @@ function _available(
     Account storage account    
 ) view returns (uint) {
     if (farm.P == 0) return 0;
-    uint R = generated(generator) / farm.D;
-    uint S = farm.S + farm.N * (R - farm.R) / farm.P;
+    uint R = farm.N * generated(generator) / farm.D;
+    uint S = farm.S + (R - farm.R) / farm.P;
     return account.R + account.P * (S - account.S) / 1e50;
 }
