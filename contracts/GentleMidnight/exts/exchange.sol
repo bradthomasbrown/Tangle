070f0816047e63a3860d9a87d773f2150cf3f6dd
Brad Brown <bradbrown@bradbrown.llc>
2023-01-21 02:18:04 -0500

    update exchange modifer, move gas on exchange

--- contracts/GentleMidnight/exts/exchange.sol
+++ contracts/GentleMidnight/exts/exchange.sol
@@ -2,17 +2,26 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.17;
 
+import '../../ERC20/ints/move.sol';
+import '../../ERC20/vars/balanceOf.sol';
+import '../../Farmable/ints/adjustGenerator.sol';
+import '../../Farmable/vars/generators.sol';
 import '../ints/insert.sol';
-import '../mods/reqChainsOrderedNoDuplicates.sol';
+import '../mods/requestChainsDistinct.sol';
+import '../structs/Input.sol';
 import '../vars/ZippySoup.sol';
 
 contract hasExtExchange is
-hasModReqChainsOrderedNoDuplicates,
+hasVarBalanceOf,
+hasVarGenerators,
+hasModRequestChainsDistinct,
 hasVarZippySoup
 {
-
-    function exchange(AER_0 calldata a0) external payable
-    reqChainsOrderedNoDuplicates(a0)
-    { insert(zs, a0, msg.sender, msg.value); }
-
+    function exchange(uint work, Request[] requests, uint gas) external payable
+    requestChainsDistinct(requests)
+    {
+        move(balanceOf, msg.sender, address(this), gas);
+        adjustGenerator(generators['tangle'], gas);
+        insert(zs, Input(work, requests, msg.sender, msg.value, zs.count++));
+    }
 }
