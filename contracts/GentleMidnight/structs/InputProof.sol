a594594e4d412e00fecdadb4a27311e5616b8ab5
Brad Brown <bradbrown@bradbrown.llc>
2023-01-22 01:12:51 -0500

    renames, redundants, and collapses

--- contracts/GentleMidnight/structs/InputProof.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: © 2023 BRAD BROWN, LLC <bradbrown@bradbrown.llc>
-// SPDX-License-Identifier: BSD-3-Clause
-pragma solidity ^0.8.17;
-
-struct InputProof {
-    bytes32[] hashes;
-    uint leafIndex;
-    uint ZSIndex;
-}
