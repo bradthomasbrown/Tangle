33e7d4062b98fea5ea0e2358a48ea8c3c49d8e3a
Brad Brown <bradbrown@bradbrown.llc>
2023-01-22 01:16:44 -0500

    rewrite mod inputsOpen

--- contracts/GentleMidnight/mods/inputsOpen.sol
+++ contracts/GentleMidnight/mods/inputsOpen.sol
@@ -2,21 +2,15 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.17;
 
-import '../structs/Stream.sol';
+import '../structs/Input.sol';
 
 contract hasModInputsOpen
 {
     modifier inputsOpen(
-        Stream[] calldata streams,
-        mapping(uint => uint) storage chunks,
+        Input[] calldata inputs,
+        mapping(uint => uint) storage chunks
     ) {
-        uint i;
-        for (; streams[i].chain != block.chainid; i++);
-        Stream calldata stream = streams[i];
-        for (i = 0; i < stream.inputs.length; i++) {
-            uint id = stream.inputs[i].id;
-            require(chunks[id / 256] & 1 << id % 256 == 0, 'input closed')
-        }
+        for (uint i; i < inputs.length; i++) require(chunks[inputs[i].id / 256] & 1 << inputs[i].id % 256 == 0, 'input closed');
         _;
     }
 }
