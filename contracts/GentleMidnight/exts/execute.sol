4e31f78c4cba9ae4513d83205767bbc185455c3e
Brad Brown <bradbrown@bradbrown.llc>
2023-01-22 01:12:15 -0500

    overhaul

--- contracts/GentleMidnight/exts/execute.sol
+++ contracts/GentleMidnight/exts/execute.sol
@@ -2,86 +2,57 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.17;
 
-import '../../ERC20/ints/move.sol';
-import '../../ERC20/vars/balanceOf.sol';
-
 import '../../Farmable/ints/adjustPoints.sol';
-import '../../Farmable/ints/adjustGenerator.sol';
-import '../../Farmable/vars/generators.sol';
+import '../../Farmable/vars/generator.sol';
 
-import '../ints/applyRefunds.sol';
-import '../ints/createNewInputs.sol';
-import '../ints/executeDirs.sol';
-import '../ints/markInputs.sol';
+import '../ints/stos.sol';
 import '../mods/inputsOpen.sol';
-import '../mods/inputsVerified.sol';
-import '../mods/auditWorks.sol';
-import '../mods/outputsNotGreaterThanInputs.sol';
-import '../mods/oneRelevantStream.sol';
-import '../structs/IO.sol';
-import '../structs/Work.sol';
-import '../structs/Proof.sol';
-import '../structs/InputProof.sol';
 import '../vars/chunks.sol';
+import '../mods/inputsVerified.sol';
 import '../vars/ZippySoup.sol';
+import '../mods/workchainIntact.sol';
+import '../mods/workSufficient.sol';
+import '../mods/followsFirstLaw.sol';
+import '../ints/processRollovers.sol';
+import '../ints/processOutputs.sol';
+import '../ints/markInputs.sol';
+import '../ints/getExecutor.sol';
 
 contract hasExtExecute is
-hasModInputsOpen,
-hasModInputsVerified,
-hasModAuditWorks,
-hasModOneRelevantStream,
-hasModOutputsNotGreaterThanInputs,
-hasVarBalanceOf,
 hasVarChunks,
-hasVarGenerators,
-hasVarZippySoup
+hasModInputsOpen,
+hasVarZippySoup,
+hasModInputsVerified,
+hasModWorkchainIntact,
+hasModWorkSufficient,
+hasModFollowsFirstLaw,
+hasVarGenerator
 {
     function execute(
         Stream[] calldata streams, 
         Work[] calldata works, 
-        Proof[] workProofs, 
-        InputProof[] inputProofs
+        Proof[] calldata workProofs, 
+        Proof[] calldata inputProofs
     ) external
-    inputsOpen(streams, chunks)
-    inputsVerified(streams, inputProofs, zs)
-    oneRelevantStream(streams)
-    // TODO: break auditWorks into workchainIntact and worksSufficient
-    auditWorks(transaction.works)
-    outputsNotGreaterThanInputs(transaction.a2s, transaction.dirs)
+    inputsOpen(stos(streams).inputs, chunks)
+    inputsVerified(stos(streams).inputs, inputProofs, zs)
+    workchainIntact(works, streams, workProofs)
+    workSufficient(works, stos(streams).inputs)
+    followsFirstLaw(stos(streams).inputs, stos(streams).outputs, stos(streams).rollovers)
     {
-        applyRefunds(transaction.a2s);
-        createNewInputs(transaction.a2s, zs);
-        executeDirs(transaction.dirs);
-        markInputs(transaction.a2s, chunks);
-        Work[] calldata works = transaction.works;
-        string[2] memory foo = ['tangle', 'native'];
-        bool yoink = works[works.length - 1].worker == msg.sender;
+        Stream calldata stream = stos(streams);
+        processRollovers(stream.rollovers, stream.inputs, zs);
+        processOutputs(stream.outputs);
+        markInputs(stream.inputs, chunks);
+        Farm storage farm = generator.farms['GentleMidnight'];
+        Input[] calldata inputs = stream.inputs;
         for (uint i; i < works.length; i++) {
-            int workScore = int(score(keccak256(abi.encode(works[i]))));
-            int yoinkedScore;
-            if (yoink) {
-                yoinkedScore += workScore * 3 / 4;
-                workScore = workScore * 1 / 4;
-            }
-            for (uint j; j < foo.length; j++) {
-                Generator storage generator = generators[foo[j]];
-                Farm storage farm = generator.farms['GentleMidnight'];
-                Account storage account = farm.accounts[works[i].worker];
-                adjustPoints(generator, farm, account, workScore);
-                if (yoink && i == works.length - 1)
-                    adjustPoints(generator, farm, account, yoinkedScore);
-            }
+            address worker = works[i].worker;
+            adjustPoints(generator, farm, farm.accounts[worker], int(score(works, worker) * 1 / 4));
+            payable(worker).transfer(sum(inputs) * 1 * 1 * score(works, worker) / score(works) / 20 / 4);
         }
-        AER_2[] calldata a2s = transaction.a2s;
-        uint tax;
-        for (uint i; i < a2s.length; i++) {
-            tax += a2s[i].refund / 20;
-        }
-        Dir[] calldata dirs = transaction.dirs;
-        for (uint i; i < dirs.length; i++) {
-            tax += dirs[i].value / 20;
-        }
-        adjustGenerator(generators['native'], tax);
+        address executor = getExecutor(works, max(inputs));
+        adjustPoints(generator, farm, farm.accounts[executor], int(score(works) * 3 / 4));
+        payable(executor).transfer(sum(inputs) * 3 * 1 / 20 / 4);
     }
-
 }
