3d0c21566cc18af844bb4f941bb4233d914fbefa
Brad Brown <bradbrown@bradbrown.llc>
2023-01-22 01:15:32 -0500

    add processRollovers()

---
+++ contracts/GentleMidnight/ints/processRollovers.sol
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: © 2023 BRAD BROWN, LLC <bradbrown@bradbrown.llc>
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.17;
+
+import '../structs/Rollover.sol';
+import '../structs/Input.sol';
+import '../ints/insert.sol';
+
+function processRollovers(
+    Rollover[] calldata rollovers,
+    Input[] calldata inputs,
+    ZippySoup storage zs
+) {
+    for (uint i; i < rollovers.length; i++) {
+        Rollover calldata rollover = rollovers[i];
+        Modifier calldata inMod = rollover.inMod;
+        Input calldata input = inputs[inMod.index];
+        Modifier[] calldata reqMods = rollover.reqMods;
+        Request[] memory requests = new Request[](reqMods.length);
+        for (uint j; j < reqMods.length; j++) {
+            Modifier calldata reqMod = reqMods[j];
+            Request calldata request = input.requests[reqMod.index];
+            requests[j] = Request(request.chain, request.value - reqMod.subtrahend);
+        }
+        insert(zs, Input(input.work, requests, input.sender, input.value - inMod.subtrahend, zs.count++));
+    }
+}
