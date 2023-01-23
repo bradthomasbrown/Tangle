5439e92bd757b8de91b587ab4aa10a9553c7a3b8
Brad Brown <bradbrown@bradbrown.llc>
2023-01-23 16:09:31 -0500

    remove trash comment

--- testing/HurtfulBeanstalk/app.mjs
+++ testing/HurtfulBeanstalk/app.mjs
@@ -6,7 +6,6 @@ import { SS } from './HelpfulCandlewax/index.mjs'
 function sendEth() {
     if (this.request.url == '/shutdown') return
     let address = this.request.url.substring(1)
-    // web3.personal.unlockAccount(web3.personal.listAccounts[0],"<password>", 15000)
     execSync(`geth --exec 'web3.personal.unlockAccount(eth.coinbase, "", 86400)' attach http://localhost:8545`)
     let result = execSync(`geth --exec 'eth.sendTransaction({ from: eth.coinbase, to: "${address}", value: web3.toWei(1, "ether") })' attach http://localhost:8545`)
     this.data = result.toString().match(/"(.*)"/)[1]
