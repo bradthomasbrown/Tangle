4db13250c6a92f7c1d78ce82aa506b7db4c2e583
Brad Brown <bradbrown@bradbrown.llc>
2023-01-23 16:09:14 -0500

    event Exchange add

--- contracts/GentleMidnight/exts/exchange.sol
+++ contracts/GentleMidnight/exts/exchange.sol
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.17;
 
+import '../events/Exchange.sol';
 import '../../ERC20/ints/move.sol';
 import '../../ERC20/vars/balanceOf.sol';
 import '../../Farmable/ints/adjustGenerator.sol';
@@ -13,12 +14,14 @@ import '../vars/ZippySoup.sol';
 contract hasExtExchange is
 hasVarZippySoup,
 hasVarBalanceOf,
-hasVarGenerator
+hasVarGenerator,
+hasEventExchange
 {
     function exchange(uint work, Request[] calldata requests, uint gas) external payable
     {
         balanceOf[msg.sender] -= gas;
         adjustGenerator(generator, gas);
         insert(zs, Input(work, requests, msg.sender, msg.value, zs.count++));
+        emit Exchange();
     }
 }
