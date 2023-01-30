30cbaaf6b02b34657c31fd663e8ed0d5a6af332d
Brad Brown <bradbrown@bradbrown.llc>
2023-01-30 15:20:52 -0500

    push setLiquidity to ext

---
+++ contracts/Tangle/exts/setLiquidity.sol
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: © 2023 BRAD BROWN, LLC <bradbrown@bradbrown.llc>
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.17;
+
+import '../../Farmable/vars/liquidity.sol';
+import '../mods/isOwner.sol';
+
+contract hasExtSetLiquidity is
+hasVarLiquidity,
+hasVarOwner,
+hasModIsOwner
+{
+    function setLiquidity(address _liquidity) external isOwner
+    {
+        liquidity = ERC20(_liquidity);
+    }
+}
