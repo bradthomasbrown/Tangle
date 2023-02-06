a72f9863186d8aeb36b1845729b928fdd2b4bcdc
Brad Brown <bradbrown@bradbrown.llc>
2023-02-05 22:07:54 -0500

    update SPDX License ID and copyright text

--- contracts/Tangle/vars/airdropAmount.sol
+++ contracts/Tangle/vars/airdropAmount.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: 0BSD
+// SPDX-FileCopyrightText: © 2023 BRAD BROWN, LLC <bradbrown@bradbrown.llc>
+// SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.17;
 
 contract hasVarAirdropAmount {
