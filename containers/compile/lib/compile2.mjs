25c7942414886ea8292029ed21341fd71e0ca7e0
Brad Brown <bradbrown@bradbrown.llc>
2023-02-05 22:05:31 -0500

    compiler json creator/recursive path traversing /thing/

---
+++ containers/compile/lib/compile2.mjs
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import { readFileSync, write, writeFileSync } from 'fs'
+import { resolve, dirname } from 'path'
+
+// let home = '/home/bradbrown' foo bar
+// let contracts = `${home}/Tangle/contracts`
+let home = '/app'
+let contracts = `${home}/contracts`
+
+let getInvolvedPaths = file => Array.from(readFileSync(file, { encoding: 'utf8'}).matchAll(/import '(.*)'/g)).map(i => resolve(dirname(file), i[1]))
+let importsToSources = (path, content) => content.split('\n').map(line => line.match('import') ? `import '${resolve(dirname(path), line.match(/import '(.*)';/)[1]).replace(`${contracts}/`, '')}';` : line ).join('\n')
+let pathToSource = path => { return { [path.replace(`${contracts}/`, '')]: { content: importsToSources(path, readFileSync(path, { encoding: 'utf8' })) }}}
+
+let start = `${contracts}/Tangle/Tangle.sol`
+let paths = []
+let check = [start]
+while (check.length) {
+    check.push(...getInvolvedPaths(check[0]).filter(path => [...paths, ...check].indexOf(path) == -1))
+    paths.push(check.shift())
+}
+let output = JSON.stringify({
+    language: 'Solidity',
+    sources: Object.assign({}, ...paths.map(path => pathToSource(path))),
+    settings: { viaIR: true, optimizer: { enabled: true, runs: 65535 }, outputSelection: { 'Tangle/Tangle.sol': { Tangle: ['abi', 'evm.bytecode.object'] }}}
+})
+writeFileSync('/tmp/tangle.json', output)
+console.log(output)
