2411d0f18e2c0a851852d34cfc63416af3c3ac3e
Brad Brown <bradbrown@bradbrown.llc>
2023-02-08 00:56:55 -0500

    this lets me view the page on my phone

--- containers/ui/vite.config.ts
+++ containers/ui/vite.config.ts
@@ -2,6 +2,9 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    server: {
+        host: true
+    },
     plugins: [
         [vue()],
         {
