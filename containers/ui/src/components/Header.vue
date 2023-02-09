1efe97ae1c631226b55197f9ee0dbff14c6f3a50
Brad Brown <bradbrown@bradbrown.llc>
2023-02-09 10:27:44 -0500

    ui header overhaul

--- containers/ui/src/components/Header.vue
+++ containers/ui/src/components/Header.vue
@@ -1,40 +1,60 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-import { Web3Provider } from '@ethersproject/providers'
-let active = ref(false)
-let hover = ref(false)
-let focus = ref(false)
-let connected = ref(false)
-let action = (e: Event) => {
-    if (connected.value) return
-    if (e.type == 'mousedown') active.value = true
-    if (e.type == 'mouseup') active.value = false
-    if (e.type == 'mouseleave') { hover.value = false; active.value = false }
-    if (e.type == 'mouseover') hover.value = true
-    if (e.type == 'keydown') active.value = true
-    if (e.type == 'keyup') active.value = false
-    if (e.type == 'focusin') focus.value = true
-    if (e.type == 'focusout') focus.value = false
-    if (e.type == 'touchstart') active.value = true
-    if (e.type == 'touchend') active.value = false
-}
-let click = async () => {
-    if (!window['ethereum']) { alert('Metamask not detected!'); return }
-    let provider = new Web3Provider(window['ethereum'])
-    await provider.send('eth_requestAccounts', [])
-    connected.value = true
-    active.value = false
-    hover.value = false
-    focus.value = false
-    window['ethereum'].on('accountsChanged', (accounts: string[]) => {
-        if (!accounts.length) connected.value = false
-    })
+<script lang="ts">
+import { ethers } from 'ethers'
+let { providers } = ethers
+let { Web3Provider } = providers
+export default {
+    data() {
+        return {
+            active: false,
+            hover: false,
+            focus: false,
+            connected: false
+        }
+    },
+    methods: {
+        action(e: Event) {
+            if (this.connected) return
+            if (e.type == 'mousedown') this.active = true
+            if (e.type == 'mouseup') this.active = false
+            if (e.type == 'mouseleave') { this.hover = false; this.active = false }
+            if (e.type == 'mouseover') this.hover = true
+            if (e.type == 'keydown') this.active = true
+            if (e.type == 'keyup') this.active = false
+            if (e.type == 'focusin') this.focus = true
+            if (e.type == 'focusout') this.focus = false
+            if (e.type == 'touchstart') this.active = true
+            if (e.type == 'touchend') this.active = false
+        },
+        async click() {
+            if (!window['ethereum']) { alert('Metamask not detected!'); return }
+            let provider = new Web3Provider(window['ethereum'])
+            await provider.send('eth_requestAccounts', [])
+            this.connected = true
+            this.active = false
+            this.hover = false
+            this.focus = false
+            window['ethereum'].on('accountsChanged', (accounts: string[]) => {
+                if (!accounts.length) this.connected = false
+            })
+        },
+        accountsChanged() {
+            let ethereum = window['ethereum']
+            if (!ethereum.selectedAddress) this.connected = false
+        }
+    },
+    mounted() {
+        let ethereum = window['ethereum']
+        if (!ethereum) return
+        if (ethereum.isConnected() && ethereum.selectedAddress)
+            this.connected = true
+        ethereum.on('accountsChanged', this.accountsChanged)
+    }
 }
 </script>
 
 <template>
     <div id="header">
-        <img draggable="false" alt="tngl-logo" src="tngl-logo.svg">
+        <img draggable="false" alt="tngl-logo" src="/tngl-logo.svg">
         tngl.io
         <div class="filler"></div>
         <button
@@ -54,7 +74,7 @@ let click = async () => {
             @keyup.enter="action"
             @focusin="action"
             @focusout="action"
-            @touchstart="action"
+            @touchstart.passive="action"
             @touchend="action"
             @click="click"
         >
