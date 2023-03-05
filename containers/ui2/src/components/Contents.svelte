93a9266e158ec93d1daac03b311b6623312f2c70
Brad Brown <bradbrown@bradbrown.llc>
2023-03-04 19:52:10 -0500

    create contents component

---
+++ containers/ui2/src/components/Contents.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { Web3 } from '../stores/Web3.mjs'
+    import Interactables from './Interactables.svelte'
+    import Connector from './Connector.svelte'
+    import Spidey from './Spidey.svelte'
+    $: spidey = !$Web3.tangle
+</script>
+
+<div>
+    <Spidey/>
+        <!-- <img src="/spidey.gif" alt="a cool gif of spiderman dancing"> -->
+    <Connector/>
+    <Interactables/>
+    <!-- {#if spidey}
+        <img src="spidey.gif" alt="a cool gif of spiderman dancing">
+    {:else}
+        <Balances/>
+        <IOs/>
+        <TradeButton/>
+    {/if} -->
+</div>
+
+<style>
+    div {
+        position: relative;
+        display: flex;
+        gap: 15px;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
