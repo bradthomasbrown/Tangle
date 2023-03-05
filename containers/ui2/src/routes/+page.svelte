1bfa6898fe7143dc253f3dd819a8e33be948aa1c
Brad Brown <bradbrown@bradbrown.llc>
2023-03-04 19:55:49 -0500

    update route +page structure

--- containers/ui2/src/routes/+page.svelte
+++ containers/ui2/src/routes/+page.svelte
@@ -1,7 +1,31 @@
 <script lang="ts">
-    import Nav from '../components/Nav.svelte';
-    import Body from '../components/Body.svelte';
+    import Logo from '../components/Logo.svelte';
+    import { fly } from 'svelte/transition';
+    import { quartInOut } from 'svelte/easing';
+    import Contents from '../components/Contents.svelte'
+    import { Web3 } from '../stores/Web3.mjs';
+    let condition = false
+    setTimeout(() => condition = true)
 </script>
 
-<Nav/>
-<Body/>
+{#if condition}
+    <div transition:fly={{
+        duration: 2000,
+        delay: 1000,
+        y: window.innerHeight / 2 - 50 - 20,
+        opacity: 1,
+        easing: quartInOut
+    }}>
+        <Logo/>
+        <Contents/>
+    </div>
+{/if}
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 15px;
+    }
+</style>
