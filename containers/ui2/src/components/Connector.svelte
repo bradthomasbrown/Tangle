28bed1a7b3f853b38f16ccd84ab6006317d353de
Brad Brown <bradbrown@bradbrown.llc>
2023-03-04 19:52:00 -0500

    add transitions to connector component

--- containers/ui2/src/components/Connector.svelte
+++ containers/ui2/src/components/Connector.svelte
@@ -1,10 +1,34 @@
 <script lang="ts">
     import Button from '@smui/button'
+    import { quartInOut } from 'svelte/easing';
+    import { fly, scale } from 'svelte/transition';
     import { Web3, getSigner } from '../stores/Web3.mjs'
+    let easing = quartInOut
+    let inParams = {  duration: 2000, delay: 1000, x: 0, 
+        y: Math.min(window.innerWidth / 50, 8) * 50 / 2,  easing }
+    let outParams = { duration: 1000, delay: 0, easing }
+    $: show = $Web3.ethereum && !$Web3.selectedAddress && !!$Web3.tangle
+    setTimeout(() => {
+        inParams = { duration: 1000, delay: 0, x: 0,
+            y: document.body.scrollHeight, easing }
+    }, 3000)
 </script>
 
-{#if $Web3.ethereum && !$Web3.selectedAddress}
-    <Button variant="unelevated" on:click="{getSigner}">
-        Connect Wallet
-    </Button>
+{#if show}
+    <div in:fly={ inParams } out:scale={ outParams }>
+        <Button variant="unelevated" on:click="{getSigner}">
+            Connect Wallet
+        </Button>
+    </div>
 {/if}
+
+<style>
+    div {
+        position: absolute;
+        top: 0;
+        width: 300px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
