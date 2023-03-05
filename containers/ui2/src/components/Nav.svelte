37f7b166c996ad403c2d3a18d19c3c90370b33da
Brad Brown <bradbrown@bradbrown.llc>
2023-03-04 19:54:47 -0500

    update nav component structure

--- containers/ui2/src/components/Nav.svelte
+++ containers/ui2/src/components/Nav.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
     import Logo from './Logo.svelte'
-    import FlexFiller from './FlexFiller.svelte'
-    import Connector from './Connector.svelte'
 </script>
 
 <div>
     <Logo/>
-    <FlexFiller/>
-    <Connector/>
 </div>
 
 <style>
     div {
         display: flex;
         align-items: center;
+        justify-content: center;
     }
 </style>
