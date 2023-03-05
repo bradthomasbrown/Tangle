7cc7e86e8dbc67fc022fc9a0a1f324da95ee44fb
Brad Brown <bradbrown@bradbrown.llc>
2023-03-04 19:53:00 -0500

    add expert mode reaction 'invalid'

--- containers/ui2/src/components/IOExpert.svelte
+++ containers/ui2/src/components/IOExpert.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
     import Switch from "@smui/switch/src/Switch.svelte";
     import FlexFiller from "./FlexFiller.svelte";
+    import { Subreq } from '../stores/Subreq.mjs'
     export let expert: boolean
+    $: invalid = $Subreq.map(subreq => subreq.gas === undefined)
+        .reduce((p, c) => p || c)
+        || $Subreq.map(subreq => !subreq.work)
+        .reduce((p, c) => p || c) ? 'invalid' : ''
 </script>
 
 <div>
-    Expert Mode
+    <span class={invalid}>Expert Mode</span>
     <FlexFiller/>
     <Switch bind:checked={expert}></Switch>
 </div>
 
 <style>
+    .invalid {
+        color: #d32f2f;
+    }
     div {
         margin-top: 16px;
         display: flex;
