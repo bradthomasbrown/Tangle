fa3964d99e3a0961b2fc38fb136db371cf56daac
Brad Brown <bradbrown@bradbrown.llc>
2023-03-07 09:26:17 -0500

    modify page route

--- containers/ui2/src/routes/+page.svelte
+++ containers/ui2/src/routes/+page.svelte
@@ -1,28 +1,42 @@
 <script lang="ts">
-    import Logo from '../components/Logo.svelte';
-    import { fly } from 'svelte/transition';
     import { quartInOut } from 'svelte/easing';
-    import Contents from '../components/Contents.svelte'
-    import { Web3 } from '../stores/Web3.mjs';
-    let condition = false
-    setTimeout(() => condition = true)
+    import { fly } from 'svelte/transition';
+    import Header from '../components/Header.svelte'
+    import Body from '../components/Body.svelte'
+    let disableSignFly = false
+    $: console.log(disableSignFly)
+    let intro = true
+    let introDrawDuration = 2000
+    let app = 0
+    let _ = false
+    setTimeout(() => _ = true)
+    setTimeout(() => intro = false, 3000)
 </script>
-
-{#if condition}
+{#if _}
     <div transition:fly={{
         duration: 2000,
-        delay: 1000,
+        delay: introDrawDuration,
         y: window.innerHeight / 2 - 50 - 20,
         opacity: 1,
         easing: quartInOut
     }}>
-        <Logo/>
-        <Contents/>
+        <Header
+            bind:disableSignFly
+            bind:app
+            bind:intro
+            bind:introDrawDuration
+        />
+        <Body
+            bind:app
+            bind:introDrawDuration
+            bind:disableSignFly
+        />
     </div>
 {/if}
 
 <style>
     div {
+        padding: 24px 16px;
         display: flex;
         flex-direction: column;
         align-items: center;
